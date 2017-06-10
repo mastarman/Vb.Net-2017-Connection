ecc65d75321a367269b6304ba8e8dd68c2cb37b6
mastar <tarman_75@yahoo.com>
2017-06-10 11:09:05 -0700

    Net Tutorial Untuk Pemula

---
+++ NetConnection/Connection.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+Imports Npgsql
+
+Module Connection
+    Dim db_string As String
+    Dim Conn1 As OleDbConnection    '' Access DB
+    Dim Conn2 As SqlConnection      '' Ms SQL Server
+    Dim Conn3 As MySqlConnection    '' Mysql
+    Dim Conn4 As NpgsqlConnection   '' PostgreSQL
+
+    Sub DBConn_Accdb() '' Connection For Access DB
+        db_string = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Net\tarman.accdb;"
+        Conn1 = New OleDbConnection(db_string)
+
+        Try
+            Conn1.Open()
+            MsgBox("DB Access Success")
+        Catch ex As OleDbException
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Sub DBConn_MsSQL()  ''COnnection For Ms SQL Server
+        db_string = "Data Source=WJT-01\SQLEXPRESS;Initial Catalog=customer;User ID=sa;Password=12345;"
+        Conn2 = New SqlConnection(db_string)
+
+        Try
+            Conn2.Open()
+            MsgBox("Ms SQL Server Success")
+        Catch ex As SqlException
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Sub DBConn_MySQL()
+        db_string = "server=localhost;user=root;password=wishimajaya;port=3307;database=ma star;"
+        Conn3 = New MySqlConnection(db_string)
+        Try
+            Conn3.Open()
+            MsgBox("MySQL Success")
+        Catch ex As MySqlException
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Sub DBConn_PgSQL()
+        db_string = "Server=localhost;Port=5432;Database=customer;username=postgres;password=wishimajaya"
+        Conn4 = New NpgsqlConnection(db_string)
+
+        Try
+            Conn4.Open()
+            MsgBox("PostgreSQL Succes")
+        Catch ex As NpgsqlException
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Module
---
+++ NetConnection/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DBConn_Accdb()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DBConn_MsSQL()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        DBConn_MySQL()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        DBConn_PgSQL()
+    End Sub
+End Class
